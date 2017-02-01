cce712aaf8658ccd14207a8d375c7e808e12fe14
Marcial Duran <marcialduran@gmail.com>
2017-01-31 23:56:14 -0300

    2 modificacion

--- prueba.sql
+++ prueba.sql
@@ -1,1 +1,2 @@
-select * from COUNTRIES
+SELECT country_id,country_name,region_id FROM countries
+WHERE country_id = 'AR'
---
+++ prueba1.sql
@@ -0,0 +1,2 @@
+SELECT country_id,country_name,region_id FROM countries
+WHERE country_id = 'AR'
